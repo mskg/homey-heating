31eb57934d098192adfd7a95508ce36ebd0f9815
Markus <markus.kling@gmail.com>
2019-02-13 19:46:47 +0100

    Add highlighting of issues in text, resolves #40

---
+++ docs/assets/highlight.js
@@ -0,0 +1,12 @@
+function highlightMatchesInString(text) {
+    return text.replace(/#(\d\d)/ig, function (m) { 
+        return '<a href="https://github.com/mskg/homey-heating/issues/' + m.substring(1) + '">' + m + '</a>'; 
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var content = window.document.getElementById("content");
+
+    content.innerHTML = highlightMatchesInString(
+        content.innerHTML);
+});
