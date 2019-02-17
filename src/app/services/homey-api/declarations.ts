0c61f284d462b72632abfdd873242fb0f96ceb93
Markus <markus.kling@gmail.com>
2019-02-17 22:44:02 +0100

    Fix: Bundle size doubled due to a wrong reference to athom-api in frontend

--- src/app/services/homey-api/declarations.ts
+++ src/app/services/homey-api/declarations.ts
@@ -11,8 +11,3 @@ export const CLASS_THERMOSTAT: string = "thermostat";
 // from Capability — target_temperature
 export const TARGET_TEMPERATURE_MIN = 4;
 export const TARGET_TEMPERATURE_MAX = 35;
-
-export {
-    IDevice, ICapability, ICapabilityInstance, IDeviceManager,
-    IZone, IZoneManager, EventHandler, StringHashMap, HomeyAPI,
-} from "athom-api";
